75ec4b13205184f2251323df8edfb63f1601a745
Wallas Souza <mallassouza21@gmail.com>
2021-09-01 16:12:37 -0300

    Projeto Criado

---
+++ src/test/java/br/com/wallasestudos/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wallasestudos.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fdc20b7301f0eca4e4a6814640bf1a438c6083b4
Wallas Souza <mallassouza21@gmail.com>
2021-09-03 15:15:57 -0300

    Teste do banco H2

--- src/main/java/br/com/wallasestudos/cursomc/domain/Categoria.java
+++ src/main/java/br/com/wallasestudos/cursomc/domain/Categoria.java
@@ -3,9 +3,16 @@ package br.com.wallasestudos.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
 	
